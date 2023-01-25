cc674910bdd2ca736749834d92db5a05dca9e489
Pedro Piva <pedro.piva@hyperlocal.com.br>
2023-01-25 15:21:13 -0300

    fix: ajustando validações

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,13 +11,17 @@ import { Header } from "../components/Header";
 import { Input } from "../components/Input";
 import * as Style from "./style";
 import InputMask from "react-input-mask";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
 export const Home = () => {
-  const [formData, setFormData] = useState<{ [x: string]: string | boolean }>(
-    {}
-  );
+  const [formData, setFormData] = useState<{
+    [x: string]: string | boolean | number;
+  }>({
+    hasPDF: true,
+    invoiceAmount: 0,
+  });
   const [errors, setErrors] = useState<{ [x: string]: boolean }>({});
-
+  const [value, setValue] = useState();
   const errorObject = {
     error: true,
     InputProps: {
@@ -29,14 +33,25 @@ export const Home = () => {
     },
   };
 
-  const OnChageValue = (name: string, value: string | boolean) => {
+  const sucessObject: { color: "success"; error?: boolean; InputProps: any } = {
+    error: false,
+    color: "success",
+    InputProps: {
+      endAdornment: (
+        <InputAdornment position="end">
+          <CheckCircleOutlineIcon color="success" />
+        </InputAdornment>
+      ),
+    },
+  };
+
+  const OnChageValue = (name: string, value: string | boolean | number) => {
     if (value !== undefined || value !== "") {
       setFormData({
         ...formData,
         [name]: value,
       });
     }
-
     ValidateInput(name, value);
   };
   const enableValidation = (name: string) => {
@@ -57,16 +72,18 @@ export const Home = () => {
     return regex.test(phone);
   };
   const nameValidator = (name: string) => {
-    var regex = new RegExp(
-      "^[A-Z][a-z]+(?:[ ][A-Z][a-z]+)*$"
-    );
+    var regex = new RegExp("^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$");
     return regex.test(name);
   };
   const emailValidator = (email: string) => {
-    var regex = new RegExp("/^[a-z0-9.]+@[a-z0-9]+.[a-z]+.([a-z]+)?$/i");
-    return regex.test(email);
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
   };
-  const ValidateInput = (name: string, value: string | boolean) => {
+  const cepValidator = (cep: string) => {
+    return cep.match(/^[0-9]{5}-[0-9]{3}$/);
+  };
+  const ValidateInput = (name: string, value: string | boolean | number) => {
     if (name === "tel") {
       if (phoneValidator(String(value))) {
         const { tel, ...rest } = errors;
@@ -77,7 +94,6 @@ export const Home = () => {
       }
     } else if (name === "name") {
       if (nameValidator(String(value))) {
-        console.log('entrou')
         const { name, ...rest } = errors;
         setErrors({
           ...rest,
@@ -92,6 +108,14 @@ export const Home = () => {
         });
         return;
       }
+    } else if (name === "CEP") {
+      if (cepValidator(String(value))) {
+        const { CEP, ...rest } = errors;
+        setErrors({
+          ...rest,
+        });
+        return;
+      }
     }
 
     setErrors({
@@ -99,10 +123,12 @@ export const Home = () => {
       [name]: true,
     });
   };
+
   useEffect(() => {
     console.log(errors);
     console.log(formData);
   }, [errors, formData]);
+
   return (
     <>
       <Header />
@@ -117,12 +143,17 @@ export const Home = () => {
               onBlur={() => enableValidation("name")}
               name="name"
               fullWidth
-              helperText={errors.name && "Nome é obrigátorio"}
+              helperText={
+                errors.name && String(formData.name).length === 0
+                  ? "Nome é obrigátorio"
+                  : errors.name && "Insira um nome valido"
+              }
               {...(errors.name ? errorObject : {})}
+              {...(!errors?.name && formData?.name ? sucessObject : {})}
             />
 
             <InputMask
-              mask="(99) 99999-9999"
+              mask="(99) 9 9999-9999"
               maskChar=" "
               onChange={(e) =>
                 OnChageValue("tel", e.target.value.replace(/[^0-9]/g, ""))
@@ -132,8 +163,13 @@ export const Home = () => {
               variant="standard"
               fullWidth
               onBlur={() => enableValidation("tel")}
-              helperText={errors.tel && "Celular é obrigátorio"}
+              helperText={
+                errors.tel && String(formData.tel).length === 0
+                  ? "Celular é obrigátorio"
+                  : errors.tel && "Por favor digite um celular válido"
+              }
               {...(errors.tel ? errorObject : {})}
+              {...(!errors?.tel && formData?.tel ? sucessObject : {})}
             >
               {
                 //@ts-ignore
@@ -148,8 +184,13 @@ export const Home = () => {
               variant="standard"
               fullWidth
               onBlur={() => enableValidation("email")}
-              helperText={errors.email && "E-mail é obrigátorio"}
+              helperText={
+                errors.email && String(formData.email).length === 0
+                  ? "E-mail é obrigátorio"
+                  : errors.email && "Por favor digite um e-mail válido"
+              }
               {...(errors.email ? errorObject : {})}
+              {...(!errors?.email && formData?.email ? sucessObject : {})}
             />
             <Input
               onChange={(e) => OnChageValue("code", e.target.value)}
@@ -164,15 +205,59 @@ export const Home = () => {
               <FormControlLabel
                 control={
                   <Checkbox
-                    onChange={(e) => OnChageValue("hasPDF", e.target.checked)}
+                    onChange={(e) => OnChageValue("hasPDF", !e.target.checked)}
                   />
                 }
                 label="Não possuo a conta de luz em PDF"
               />
             </div>
-            <Button variant="outlined" color="primary" size="large">
-              Enviar conta de luz
-            </Button>
+            {formData.hasPDF ? (
+              <>
+                <input type="file" style={{ display: "none" }} />
+                <Button variant="outlined" color="primary" size="large">
+                  Enviar conta de luz
+                </Button>
+              </>
+            ) : (
+              <>
+                <Input
+                  onChange={(e) =>
+                    OnChageValue(
+                      "invoiceAmount",
+                      e.target.value.replace(/[^0-9]/g, "")
+                    )
+                  }
+                  type="number"
+                  name="invoiceAmount"
+                  label="Valor médio da conta de luz *"
+                  variant="standard"
+                  fullWidth
+                />
+                <InputMask
+                  mask="99999-999"
+                  maskChar=" "
+                  onChange={(e) => OnChageValue("CEP", e.target.value)}
+                  name="CEP"
+                  label="CEP *"
+                  fullWidth
+                  variant="standard"
+                  onBlur={() => enableValidation("CEP")}
+                  helperText={
+                    errors.CEP && String(formData.CEP).length === 0
+                      ? "CEP é obrigátorio"
+                      : errors.CEP && "Por favor digite um CEP válido"
+                  }
+                  {...(errors.CEP ? errorObject : {})}
+                  {...(!errors?.CEP && formData?.CEP ? sucessObject : {})}
+                >
+                  {
+                    //@ts-ignore
+                    (props) => <Input {...props}></Input>
+                  }
+                </InputMask>
+              </>
+            )}
+
             <p className="description">
               Arquivo suportado: <strong>PDF</strong>
             </p>
